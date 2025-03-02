fdfab4489cde65edf6f1cd531984b5dd2e44da6b
Htet Aung Shine <has@mit.com.mm>
2025-03-02 04:23:32 +0400

    Fixed Google Search Console verification

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
-    path("googleca93744ea4f1f4c3.html", TemplateView.as_view(template_name="googleca93744ea4f1f4c3.html"), name="google-site-verification"),
+    re_path(r'^googleca93744ea4f1f4c3.html$', serve, {'document_root': settings.STATIC_ROOT, 'path': 'googleca93744ea4f1f4c3.html'}),
+    re_path(r'^google(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 # Serve uploaded files (resume)
