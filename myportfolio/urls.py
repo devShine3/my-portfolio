b3ffd853cabbe129caef88bbc2540a0b7b138f18
Htet Aung Shine <has@mit.com.mm>
2025-03-02 04:14:18 +0400

    Updated myportfolio urls from portfolio urls

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path("googleca93744ea4f1f4c3.html", TemplateView.as_view(template_name="googleca93744ea4f1f4c3.html"), name="google-site-verification"),
 ]
 
 # Serve uploaded files (resume)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import home
-from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('', home, name='home'),
-    path("googleca93744ea4f1f4c3.html", TemplateView.as_view(template_name="googleca93744ea4f1f4c3.html"), name="google-site-verification"),
 ]
